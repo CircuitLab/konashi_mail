f89d95f08e8878afe1dd53938df99f522ddc1c50
daichi <prinum101@gmail.com>
2013-05-23 02:00:42 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KonashiMail::Application.config.secret_token = 'bdae022655682d98bffac1924f06c15254f755eb67cf67664b35a57b19ad629ac427ba65b945ffc43b29fc61b1fe49adde35ec2800f90c04c8a038365953b1e2'
